f6c5208714e26839bc2d5adaa4ed02857aadf987
Kazeem Erinfolami <erinfolamibolaji2010@gmail.com>
2021-05-27 22:07:36 +0100

    commit

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -39,9 +39,41 @@ function Home() {
                 </div>
                 <SideImg1 />
             </div>
-            <div className="Home-Services--container">
+            {/* <div className="Home-Services--container">
                 <div className="Home-Services--into">What We are into</div>
+                <div className="Home-Services--content">
+                    <div><span className="iconify Home-service--icons" data-icon="carbon:wheat" data-inline="false"></span></div>
+                    <div><span className="iconify Home-service--icons" data-icon="emojione-monotone:ear-of-corn" data-inline="false"></span></div>
+                    <div><span className="iconify Home-service--icons" data-icon="tabler:plant-2" data-inline="false"></span></div>
+                    <div><span className="iconify Home-service--icons" data-icon="whh:cow" data-inline="false"></span></div>
+                    <div><span className="iconify Home-service--icons" data-icon="vs:pig" data-inline="false"></span></div>
+                    <div><span className="iconify Home-service--icons" data-icon="emojione-monotone:tropical-fish" data-inline="false"></span></div>
+                    <div><span className="iconify Home-service--icons" data-icon="carbon:apple" data-inline="false"></span></div>
+                    <div><span className="iconify Home-service--icons" data-icon="la:fish" data-inline="false"></span></div>
+                    <div><span className="iconify Home-service--icons" data-icon="emojione-monotone:kiwi-fruit" data-inline="false"></span></div>
+                    <div><span className="iconify Home-service--icons" data-icon="mdi:fruit-grapes-outline" data-inline="false"></span></div>
+                    <div><span className="iconify Home-service--icons" data-icon="tabler:meat" data-inline="false"></span></div>
+                    <div><span className="iconify Home-service--icons" data-icon="mdi:seed" data-inline="false"></span></div>
+                </div>
                 <ProductServices />
+            </div> */}
+            <div className="Home-Services--midcontainer">
+                <div className="Home-Services--midinto">What We are into</div>
+                <div className="Home-Services--midcontent">
+                    <div><span className="iconify Home-service--icons" data-icon="carbon:wheat" data-inline="false"></span></div>
+                    <div><span className="iconify Home-service--icons" data-icon="emojione-monotone:ear-of-corn" data-inline="false"></span></div>
+                    <div><span className="iconify Home-service--icons" data-icon="tabler:plant-2" data-inline="false"></span></div>
+                    <div><span className="iconify Home-service--icons" data-icon="whh:cow" data-inline="false"></span></div>
+                    <div><span className="iconify Home-service--icons" data-icon="vs:pig" data-inline="false"></span></div>
+                    <div><span className="iconify Home-service--icons" data-icon="emojione-monotone:tropical-fish" data-inline="false"></span></div>
+                    <div><span className="iconify Home-service--icons" data-icon="carbon:apple" data-inline="false"></span></div>
+                    <div><span className="iconify Home-service--icons" data-icon="la:fish" data-inline="false"></span></div>
+                    <div><span className="iconify Home-service--icons" data-icon="emojione-monotone:kiwi-fruit" data-inline="false"></span></div>
+                    <div><span className="iconify Home-service--icons" data-icon="mdi:fruit-grapes-outline" data-inline="false"></span></div>
+                    <div><span className="iconify Home-service--icons" data-icon="tabler:meat" data-inline="false"></span></div>
+                    <div><span className="iconify Home-service--icons" data-icon="mdi:seed" data-inline="false"></span></div>
+                </div>
+
             </div>
             <MapItem />
             <div className="Home--Form-container">
